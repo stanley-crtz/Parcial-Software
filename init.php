48dd4c075c16bb3067cbd2bf27c886294c01abfe
Johansson Rodriguez <jrodriguez@solucionesroots.com>
2021-12-10 20:56:49 -0600

    change

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
     include "Database.php";
 
     define("DB_HOST", "db");
-    define("DB_NAME", "Parcial-Software");
+    define("DB_NAME", "parcial");
     define("DB_USER", "root");
     define("DB_PASS", "r00tadmin");
     define("BASE_URL", "http://localhost:8080/");
