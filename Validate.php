a8be76ef79577088bf7220221af7fd5bdca659e0
Johansson Rodriguez <jrodriguez@solucionesroots.com>
2021-12-10 21:59:48 -0600

    change

---
+++ Validate.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Validate
+    {
+        
+        public static function scape($input) {
+
+            $input = trim($input);
+            $input = stripcslashes($input);
+            $input = htmlentities($input, ENT_QUOTES);
+
+            return $input;
+
+        }
+
+        public function filterEmail($email)
+        {
+            return filter_var($email, FILTER_VALIDATE_EMAIL);
+        }
+
+    }
+    
+
+?>
--- init.php
+++ init.php
@@ -1,11 +1,17 @@
 <?php
     
-    include "Database.php";
+    spl_autoload_register(
+        function ($class) {
+            require $class.'.php';
+        }
+    );
 
     define("DB_HOST", "db");
     define("DB_NAME", "parcial");
     define("DB_USER", "root");
     define("DB_PASS", "r00tadmin");
     define("BASE_URL", "http://localhost:8080/");
+
+    $validate = new Validate;
 
 ?>
