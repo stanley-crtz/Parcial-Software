22ab7556143cc575029e7001747be79a342d00d4
Johansson Rodriguez <jrodriguez@solucionesroots.com>
2021-12-10 20:52:40 -0600

    change method

--- Database.php
+++ Database.php
@@ -5,7 +5,7 @@
         public $pdo;
         public static $instance;
         
-        protected function __construct() {
+        public function __construct() {
             $this->pdo = new PDO("mysql:host=".DB_HOST."; dbname=".DB_NAME, DB_USER, DB_PASS);
         }
 
